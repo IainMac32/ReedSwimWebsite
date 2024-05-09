2126548d83a1c4afa55f86eaf9fba376ff1dc71c
Iain Macdonald <65032680+IainMac32@users.noreply.github.com>
2024-05-09 16:30:38 -0400

    new email

--- src/components/CalendarContact.js
+++ src/components/CalendarContact.js
@@ -35,7 +35,7 @@ const Calendar = () => {
 
         <p>For booking or questions <br/>
           Contact Reed: <br/>
-          905-808-1123 <br/> OR <br/>
+          
           mac<w>swim</w>school<w>oakville</w>@gmail.com
         </p>
       </div>
