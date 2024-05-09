7fdb4d2a83114eea0e1ee84fa40f3f59df00c3ba
Iain Macdonald <65032680+IainMac32@users.noreply.github.com>
2024-05-09 16:23:39 -0400

    new images

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = () => {
       <div className="about-me-content">
         <img src={lifeguardImage} alt="Lifeguard" className="lifeguard-image" />
         <div className="text-container">
-          <p>Hey there! I'm Reed, a high school student who's also a Certified Lifeguard with a passion for swimming. I've been immersed in the world of aquatics for as long as I can remember, and now I'm thrilled to share my love for the water through my own swim school. Whether you're just dipping your toes or diving into competitive strokes, I'm here to make every lesson a splashin' success!</p>
+          <p>Hey there! I'm Reed, a high school student who's also a Certified Lifeguard with a passion for swimming. I've been immersed in the world of aquatics for as long as I can remember, and I'm thrilled to share my love for the water through my own swim school. Whether you're just dipping your toes or diving into competitive strokes, I'm here to make every lesson a splashin' success!</p>
         </div>
       </div>
     </section>
